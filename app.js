88f274b91ca7072fe0814cdf75cb8a9829d2cea4
Julian Chung <juliancwt77@gmail.com>
2016-10-11 17:13:45 +0800

    add express

---
+++ app.js
@@ -0,0 +1,20 @@
+// import express
+var express = require('express')
+
+// instantiating the express server
+var app = express()
+var port = 3000
+
+// require path-name
+
+
+// middleware for all requests
+// running express with ejs
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+app.listen(port)
+console.log('server running at http://localhost:' + port + '/')
